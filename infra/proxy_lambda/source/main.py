c9ba6ddba5e4eafb3815cab428c9f1f6281b1381
anton.cherpak <cherpak.anton@gmail.com>
2025-11-26 12:32:44 -0500

    17

--- infra/proxy_lambda/source/main.py
+++ infra/proxy_lambda/source/main.py
@@ -1,16 +1,25 @@
 from runner.executor import run_python_tests
 from runner.logger import log
 
-def main(region="US", locale="en-US", branch="main", repo_url="https://github.com/your-org/your-tests.git"):
+def main(params=None):
     """
     Entry point for scheduled or Cloud Build-triggered execution.
     """
-    log(f"Trigger received. Repo: {repo_url}, Branch: {branch}, Region: {region}, Locale: {locale}")
+    if params is None:
+        params = {
+            "region": "US",
+            "locale": "en-US",
+            "branch": "main",
+            "repo_url": "https://github.com/your-org/your-tests.git"
+        }
+    
+    # log(f"Trigger received. Repo: {repo_url}, Branch: {branch}, Region: {region}, Locale: {locale}")
 
     try:
-        output = run_python_tests(repo_url, branch, region, locale)
+        # output = run_python_tests(repo_url, branch, region, locale)
+        print(params)    
         log("Execution finished.")
-        log(f"Output: {output}")
+        log(f"Output: {params}")
         return output
     except Exception as e:
         log(f"ERROR: {e}")
