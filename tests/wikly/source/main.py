5eabf98aca8555a4b0f953cbb71bbdf8beaf9f4b
anton.cherpak <cherpak.anton@gmail.com>
2025-11-29 15:48:21 -0500

    35

--- tests/wikly/source/main.py
+++ tests/wikly/source/main.py
@@ -1,1 +1,9 @@
-print("start qa script")
+from playwright.sync_api import sync_playwright
+
+def test_homepage_status():
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        page.goto("https://example.com")
+        assert page.status == 200
+        browser.close()
