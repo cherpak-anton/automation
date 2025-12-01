3ded12d21b3aa8b506ff8620927ff180d5a185b7
anton.cherpak <cherpak.anton@gmail.com>
2025-12-01 18:31:42 -0500

    113

--- tests/wikly/source/test_main.py
+++ tests/wikly/source/test_main.py
@@ -5,6 +5,7 @@ def test_homepage_status():
     with sync_playwright() as p:
         browser = p.chromium.launch()
         page = browser.new_page()
-        page.goto("https://example.com")
-        assert page.status == 200
+        response = page.goto("https://example.com")
+        assert response.status == 200
+        
         browser.close()
